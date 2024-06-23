7a0d5b10d835eaf2dd866d3b1acd1ee6c9894f21
Tomi <tlapietra98@gmail.com>
2024-06-23 19:23:29 -0300

    clase 08/06/24 parte 1
    aprendiendo firebase

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -3,8 +3,8 @@ import Loader from "../Loader/Loader"
 import ItemList from "../ItemList/ItemList"
 import { CartContext } from "../../context/CartContext"
 import { useParams } from "react-router-dom"
+import { collection, getDocs, doc, query, where, getDoc, getFirestore } from "firebase/firestore"
 import "./ItemListContainer.css"
-import ItemDetailContainer from "../ItemDetailContainer/ItemDetailContainer"
 
 const ItemListContainer = ({greeting}) => {
 
@@ -14,13 +14,42 @@ const ItemListContainer = ({greeting}) => {
 
   const { items } = useContext(CartContext)
 
-  console.log(items)
+  //console.log(items)
+
+  const [productsFiltered, setProductsFiltered] = useState([])
+
 
   useEffect(
     () => {
 
       const fetchData = async () => {
         try{
+
+
+          //con firebase
+          const db = getFirestore()
+
+          if (id === undefined){
+            console.log("ID is undefined")
+
+            const docsRef = collection(db, "products")
+            const querySnaphot = await getDocs(docsRef)
+            setProductsFiltered(querySnaphot.docs.map(doc => ({id:doc.id, ...doc.data()})))
+            querySnaphot.forEach((doc)=>{console.log(doc.id, "=>", doc.data())})
+          }
+          else{
+            console.log("ID is: ", id)
+
+            const q = query(collection(db, "products"), where("category", "==", id))
+            const querySnaphot = await getDocs(q)
+            setProductsFiltered(querySnaphot.docs.map(doc => ({id:doc.id, ...doc.data()})))
+            querySnaphot.forEach((doc)=>{console.log(doc.id, "=>", doc.data())})
+          }
+          
+
+
+
+          //con json
           const response = await fetch ("/data/products.json")
           const data = await response.json()
           
@@ -28,6 +57,8 @@ const ItemListContainer = ({greeting}) => {
             setProducts(data)
             :
             setProducts(data.filter(product => product.category == id))
+
+
         }
         catch(error){
           console.log("Error while obtaining products", error)
@@ -42,8 +73,6 @@ const ItemListContainer = ({greeting}) => {
     <div className="contentContainer">
       
       <h3 className='contentTitle'>{greeting}</h3>
-
-      {/* <ItemDetailContainer itemID={1001}/> */}
 
       {products.length == 0 ? 
         <Loader/> 
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,18 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App.jsx"
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBgim0c-a3Kqla247JQvNZhnQ63cCOES7w",
+  authDomain: "coderreactjs57760.firebaseapp.com",
+  projectId: "coderreactjs57760",
+  storageBucket: "coderreactjs57760.appspot.com",
+  messagingSenderId: "681928213924",
+  appId: "1:681928213924:web:630b299c2b5db062e82d31"
+};
+
+initializeApp(firebaseConfig);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
