79f93c8b78e026c841a7013f1dfae37feb87f259
Tomi <tlapietra98@gmail.com>
2024-06-24 01:05:01 -0300

    clase 08/06/24 parte 5
    orders! funciona, igual no le meti magia para completar uno los datos todavia
    
    falta

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,18 +1,39 @@
 import { useContext } from "react"
 import { CartContext } from "../../context/CartContext"
 import CartItem from "../CartItem/CartItem"
+import { collection , addDoc, getFirestore} from "firebase/firestore"
 
 function Cart (){
 
-    const { cartItems, totalCartPrice } = useContext(CartContext)
+    const { cartItems, totalCartPrice, clearCart } = useContext(CartContext)
 
     console.log(cartItems)
+
+
+    const addPurchaseOrder = async () => {
+        
+        const order = {
+            buyer:{
+                name: "Fulano de Tal",
+                phone: "1234567890",
+                email: "fulanito98@hotmail.com"
+            },
+            items: cartItems,
+            total: totalCartPrice
+        }
+        
+        const db = getFirestore()
+        const docRef = collection(db,"orders")
+        await addDoc(docRef, order)
+        clearCart()
+    }
 
     if (cartItems.length){
         return (
             <div>
                 {cartItems.map(item => <CartItem key={item.id} cItem={item} />)}
                 <p>Total: {totalCartPrice}</p>
+                <button onClick={addPurchaseOrder}>Confirm purchase</button>
             </div>
         )
     }
